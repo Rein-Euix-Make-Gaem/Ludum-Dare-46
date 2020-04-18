6e078d6315e6d017f4c5441526a65439f991a9db
reinaurre <rein@rein-online.org>
2020-04-18 16:37:52 -0700

    Large and Small holes finished except oxygen effect.  Creature now explodes.  Added game manager with scene-loader and lose state.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Animator cameraAnimator;
+
+    public void SmallShake()
+    {
+        cameraAnimator.SetTrigger("smallShake");
+    }
+
+    public void BigShake()
+    {
+        cameraAnimator.SetTrigger("bigShake");
+    }
+}
