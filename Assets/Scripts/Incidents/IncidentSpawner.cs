fc86bc6e8a05450cd03a19d27a5b9b9735dc5622
reinaurre <rein@rein-online.org>
2020-04-18 01:20:49 -0700

    Starting work on incident system

---
+++ Assets/Scripts/Incidents/IncidentSpawner.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Interactions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncidentSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
