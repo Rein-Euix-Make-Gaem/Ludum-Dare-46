32775a96145d18e25413208a3910d9c0f5475509
Chris Hutchinson <chris@cshutchinson.com>
2020-04-18 14:08:55 -0400

    interaction hold time; proxied interactions; fixed door interaction

---
+++ Assets/Scripts/RadialProgress.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class RadialProgress : MonoBehaviour
+{
+    private Image image;
+
+    public float progress = 0;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = progress;
+    }
+}
