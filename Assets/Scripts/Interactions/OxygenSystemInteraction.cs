5139b8ec4601b3e4146a7241a61f5a710405d529
Evan Witt <evanawitt@gmail.com>
2020-04-19 15:57:12 -0700

    Merge with master.

--- Assets/Scripts/Interactions/OxygenSystemInteraction.cs
+++ Assets/Scripts/Interactions/OxygenSystemInteraction.cs
@@ -10,26 +10,31 @@ public class OxygenSystemInteraction : ToggleInteraction
 
     private bool oxygenEnabled = false;
 
+    public string activatedEvent = "";
+    FMOD.Studio.EventInstance activatedSound;
+
+
+    private Awake(){
+        activatedSound = FMODUnity.RuntimeManager.CreateInstance(activatedEvent);
+    }
+
     protected override void OnInteract(ref InteractionEvent ev)
     {
         base.OnInteract(ref ev);
 
         this.oxygenEnabled = !this.oxygenEnabled;
 
+        if (this.oxygenEnabled){        
+            activatedSound.start();
+        }
+        else {
+            //FMODUnity.RuntimeManager.PlayOneShot("event:/OxygenDeactivated");
+        }
+
         GameManager.Instance.SetOxygenProduction(this.oxygenEnabled);
 
         // If oxygen is enabled, disable distractions.
         GameObject.FindGameObjectWithTag("CreatureRoom").GetComponent<CreatureAttitudeManager>().SetDistractionsEnabled(!this.oxygenEnabled);
-    
-        if (this.oxygenEnabled){
-            FMODUnity.RuntimeManager.PlayOneShot("event:/OxygenActivated");
-        }
-        else {
-            FMODUnity.RuntimeManager.PlayOneShot("event:/OxygenDeactivated");
-        }
-    
-    
-    
     
     }
 }
