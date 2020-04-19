071f22811627b9274754e9b6c8a134c297cea327
Evan Witt <evanawitt@gmail.com>
2020-04-18 19:28:55 -0700

    FMOD plugin initialized. Background music and one jump beep.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -84,6 +84,8 @@ public class PlayerController : MonoBehaviour
             var jumpVelocity = new Vector3(body.velocity.x, jumpSpeed, body.velocity.z);
 
             body.velocity = jumpVelocity;
+
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Button Press");
         }
     }
     
@@ -104,11 +106,15 @@ public class PlayerController : MonoBehaviour
     {
         this.IsCarryingPatch = true;
         this.CarriedLargePatch.SetActive(true);
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Button Press");
     }
 
     public void DropLargePatch()
     {
         this.IsCarryingPatch = false;
         this.CarriedLargePatch.SetActive(false);
+
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Button Press");
     }
 }
