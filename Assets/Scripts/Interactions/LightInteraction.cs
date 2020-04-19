8b3e4c3fc12be3e6a5c1f270ce7b95907ea73c12
Chris Hutchinson <chris@cshutchinson.com>
2020-04-18 21:18:30 -0400

    light puzzle game; added post processing configs

---
+++ Assets/Scripts/Interactions/LightInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Interactions
+{
+    public class LightInteraction : ToggleInteraction
+    {
+        public LightController lightController;
+        public float intensity = 10f;
+
+        protected override void OnInteract(ref InteractionEvent ev)
+        {
+            base.OnInteract(ref ev);
+
+            Toggle(toggled);
+        }
+
+        public void Toggle(bool value)
+        {
+            toggled = value;
+            lightController.intensity = value ? intensity : 0f;
+        }
+    }
+}
