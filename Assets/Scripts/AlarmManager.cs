44d2837998896b7a533e77210e310d4807497397
Chris Hutchinson <chris@cshutchinson.com>
2020-04-20 03:25:55 -0400

    fix particle read/write error; remove unused assets; sync lights to klaxon audio

---
+++ Assets/Scripts/AlarmManager.cs
@@ -0,0 +1,54 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmManager : MonoBehaviour
+{
+    public AudioSource alarmSound;
+    public float pulsesPerSecond = 0.7f;
+    public bool syncToAudio = true;
+    public Color color = Color.red;
+
+    private float time;
+
+    private void Start()
+    {
+        if (syncToAudio)
+        {
+            pulsesPerSecond = 1f / alarmSound.clip.length;
+        }
+    }
+
+    void Update()
+    {
+        if (GameManager.Instance.IsAlarmActive) 
+        {
+            if (!alarmSound.isPlaying)
+            {
+                alarmSound.Play(0);
+                time = 0;
+            }
+
+            time += Time.deltaTime;
+
+            var frequency = pulsesPerSecond;
+            var angle = 2 * Mathf.PI;
+            var alpha = (Mathf.Sin(frequency * angle * time) + 1) * 0.5f;
+
+            var from = Color.red * 0.1f;
+            var alarmColor = Color.Lerp(from, color, alpha);
+
+            GameManager.Instance.AlarmColor = alarmColor;
+        }
+        else
+        {
+            if (alarmSound.isPlaying)
+            {
+                alarmSound.Stop();
+            }
+
+            GameManager.Instance.AlarmColor = Color.black;
+        }
+    }
+}
