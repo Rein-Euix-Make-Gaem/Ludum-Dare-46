0c1a619868e24f59052c0e52d95ba01983946c1f
Chris Hutchinson <chris@cshutchinson.com>
2020-04-19 20:07:17 -0400

    fixed materials; alarm light sync

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -7,33 +7,49 @@ public class LightController : MonoBehaviour
     public float intensity = 1f;
     public float lightIntensityMultiplier = 10f;
     public float responseTime = 25;
+    public bool inheritIntesity = true;
     public bool on = true;
 
     private Light attachedLight;
     private MeshRenderer bulbRenderer;
     private Material bulbMaterial;
+    private Color currentColor;
     private float currentIntensity = 0f;
 
     public void Start()
     {
-        bulbRenderer = bulb.GetComponent<MeshRenderer>();
-        bulbMaterial = new Material(bulbRenderer.sharedMaterial);
-        bulbMaterial.color = bulbColor;
-        bulbRenderer.material = bulbMaterial;
-        attachedLight = GetComponentInChildren<Light>();
+        if (bulb != null)
+        {
+            bulbRenderer = bulb.GetComponent<MeshRenderer>();
+            bulbMaterial = new Material(bulbRenderer.sharedMaterial);
+            bulbMaterial.color = bulbColor;
+            bulbRenderer.material = bulbMaterial;
+        }
+
+        attachedLight = GetComponent<Light>() ?? GetComponentInChildren<Light>();
+
+        if (attachedLight != null && inheritIntesity)
+        {
+            intensity = attachedLight.intensity;
+        }
     }
 
     public void Update()
     {
         var targetIntensity = on ? intensity : 0f;
 
+        currentColor = bulbColor;
         currentIntensity = Mathf.Lerp(currentIntensity, targetIntensity, Time.deltaTime * responseTime);
-        bulbMaterial.color = bulbColor;
-        bulbMaterial.SetColor("_EmissionColor", bulbColor * currentIntensity);
+
+        if (bulbMaterial != null)
+        {
+            bulbMaterial.color = currentColor;
+            bulbMaterial.SetColor("_EmissionColor", currentColor * currentIntensity);
+        }
 
         if (attachedLight != null)
         {
-            attachedLight.color = bulbColor;
+            attachedLight.color = currentColor;
             attachedLight.intensity = currentIntensity * lightIntensityMultiplier;
         }
     }
